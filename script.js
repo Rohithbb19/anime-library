d156b5a4a6f610b04590fa1a9ccbbb6102e33da2
Rohithbb19 <rohithbb19@gmail.com>
2022-07-15 14:18:36 +0530

    mod

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let searchbar=document.getElementById("searchbar")
 let button=document.getElementById("button")
 let result=document.getElementById("result")
+let nodata=document.getElementById("nodata")
 
 button.addEventListener("click",search)
 searchbar.addEventListener("keydown",(event)=>{
@@ -14,6 +15,11 @@ async function search(){
     let responce = await data.json()
     result.innerHTML=""
 
+    if(responce.data.length===0){
+        nodata.innerHTML=`<p>no data found</p>`
+    }
+    
+    else{
     for(let i=0;i<=responce.data.length;i++){
         result.innerHTML+=`<a href="${responce.data[i].url}" target="_blank" class="card">
         <div class="warper" style="background-image:url(${responce.data[i].images.jpg.image_url})">
@@ -22,11 +28,7 @@ async function search(){
         <p>${responce.data[i].episodes} Episodes</p>
         </a>
         `
+        }
     }
-    
-    
-    
-    
-    
-    // result.innerHTML=responce.data[0].title_english
-}
+
+    }
